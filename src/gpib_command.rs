5b84d8d3672799177866cd8dc5f55180f4cec90c
Valerii Klachkov <vklachkov@pm.me>
2025-11-09 11:29:00 +0100

    serialize GPIBCommand and write round trip tests

--- src/gpib_command.rs
+++ src/gpib_command.rs
@@ -1,6 +1,5 @@
-/// GPIB commands (messages with ATN) that a GRiD laptop can send
-/// to a hard drive or floppy drive.
-///
+/// GPIB commands (messages with ATN) that a GRiD laptop can send to a device.
+/// 
 /// Commands are taken from the standard, section 2.13.7.1 Interface Messages.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum GPIBCommand {
@@ -56,6 +55,22 @@ impl From<u8> for GPIBCommand {
     }
 }
 
+impl Into<u8> for GPIBCommand {
+    fn into(self) -> u8 {
+        match self {
+            GPIBCommand::DCL => 0b0001_0100,
+            GPIBCommand::SDC => 0b0000_0100,
+            GPIBCommand::SPE => 0b0001_1000,
+            GPIBCommand::SPD => 0b0001_1001,
+            GPIBCommand::UNL => 0b0011_1111,
+            GPIBCommand::UNT => 0b0101_1111,
+            GPIBCommand::MLA(val) => 0b0010_0000 | (val & 0b0001_1111),
+            GPIBCommand::MTA(val) => 0b0100_0000 | (val & 0b0001_1111),
+            GPIBCommand::Unsupported(val) => val,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -79,5 +94,103 @@ mod tests {
         assert_eq!(GPIBCommand::from(0x19), GPIBCommand::SPD);
 
         assert_eq!(GPIBCommand::from(0x60), GPIBCommand::Unsupported(0x60));
+    }
+
+    #[test]
+    fn serialize_all_variants() {
+        let byte: u8 = GPIBCommand::DCL.into();
+        assert_eq!(byte, 0x14);
+        let byte: u8 = GPIBCommand::SDC.into();
+        assert_eq!(byte, 0x04);
+
+        for i in 0..31 {
+            let byte: u8 = GPIBCommand::MLA(i).into();
+            assert_eq!(byte, 0x20 | i);
+        }
+        let byte: u8 = GPIBCommand::UNL.into();
+        assert_eq!(byte, 0x3f);
+
+        for i in 0..31 {
+            let byte: u8 = GPIBCommand::MTA(i).into();
+            assert_eq!(byte, 0x40 | i);
+        }
+        let byte: u8 = GPIBCommand::UNT.into();
+        assert_eq!(byte, 0x5f);
+
+        let byte: u8 = GPIBCommand::SPE.into();
+        assert_eq!(byte, 0x18);
+        let byte: u8 = GPIBCommand::SPD.into();
+        assert_eq!(byte, 0x19);
+
+        let byte: u8 = GPIBCommand::Unsupported(0x60).into();
+        assert_eq!(byte, 0x60);
+    }
+
+    #[test]
+    fn round_trip_dcl() {
+        let cmd = GPIBCommand::DCL;
+        let byte: u8 = cmd.into();
+        let round_trip = GPIBCommand::from(byte);
+        assert_eq!(cmd, round_trip);
+    }
+
+    #[test]
+    fn round_trip_sdc() {
+        let cmd = GPIBCommand::SDC;
+        let byte: u8 = cmd.into();
+        let round_trip = GPIBCommand::from(byte);
+        assert_eq!(cmd, round_trip);
+    }
+
+    #[test]
+    fn round_trip_spe() {
+        let cmd = GPIBCommand::SPE;
+        let byte: u8 = cmd.into();
+        let round_trip = GPIBCommand::from(byte);
+        assert_eq!(cmd, round_trip);
+    }
+
+    #[test]
+    fn round_trip_spd() {
+        let cmd = GPIBCommand::SPD;
+        let byte: u8 = cmd.into();
+        let round_trip = GPIBCommand::from(byte);
+        assert_eq!(cmd, round_trip);
+    }
+
+    #[test]
+    fn round_trip_unl() {
+        let cmd = GPIBCommand::UNL;
+        let byte: u8 = cmd.into();
+        let round_trip = GPIBCommand::from(byte);
+        assert_eq!(cmd, round_trip);
+    }
+
+    #[test]
+    fn round_trip_unt() {
+        let cmd = GPIBCommand::UNT;
+        let byte: u8 = cmd.into();
+        let round_trip = GPIBCommand::from(byte);
+        assert_eq!(cmd, round_trip);
+    }
+
+    #[test]
+    fn round_trip_mla() {
+        for i in 0..31 {
+            let cmd = GPIBCommand::MLA(i);
+            let byte: u8 = cmd.into();
+            let round_trip = GPIBCommand::from(byte);
+            assert_eq!(cmd, round_trip);
+        }
+    }
+
+    #[test]
+    fn round_trip_mta() {
+        for i in 0..31 {
+            let cmd = GPIBCommand::MTA(i);
+            let byte: u8 = cmd.into();
+            let round_trip = GPIBCommand::from(byte);
+            assert_eq!(cmd, round_trip);
+        }
     }
 }
