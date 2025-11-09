42d3d88642f886ec6d2344506e6bbc0f9c20d821
Valerii Klachkov <vklachkov@pm.me>
2025-11-09 14:21:38 +0100

    custom rt logger from Cursor

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 fn main() {
     //
 
-    logger::configure();
+    logger::init();
 
-    log::info!("BlackGPiB v{VERSION} started");
+    info!("BlackGPiB v{} started", VERSION);
 
-    log::debug!("Reset all pins to Z-State...");
-    gpib_gpio::reset_all();
+    debug!("Reset all pins to Z-State...");
+    // gpib_gpio::reset_all();
 
     //
 }
