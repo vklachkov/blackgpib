2294cb28f9244896db31fe897e3e077e2a13bf09
Valerii Klachkov <vklachkov@pm.me>
2025-12-13 18:52:12 +0000

    implement reset and format in DeviceController

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,115 +1,191 @@
 mod disk_request;
 
-use std::fs;
+use std::io;
 
-use crate::{gpib_command::GPIBCommand, listener::Listener, talker::Talker, utils::busy_wait};
+use crate::{gpib_command::GPIBCommand, listener::Listener, talker::Talker};
+
+const SECTOR_SIZE: usize = 512;
 
 pub struct DeviceController {
     address: u8,
+    buffer: Vec<u8>,
 }
 
 impl DeviceController {
     pub fn new(address: u8) -> Self {
-        Self { address }
-    }
-
-    pub fn start(self) {
-        let mut buffer: Vec<u8> = Vec::with_capacity(512);
-
-        let image = fs::read("MS-DOS 2.00A.img").unwrap();
-
-        for i in 0..(image.len() / 512) {
-            self.write_sector(&image[i * 512..i * 512 + 512], i as _, &mut buffer);
+        Self {
+            address,
+            buffer: Vec::with_capacity(512),
         }
     }
 
-    fn write_sector(&self, sector_data: &[u8], sector_number: u32, buffer: &mut Vec<u8>) {
+    pub fn reset_device(&self) {
+        let mut talker = Talker::new();
+        talker.send_command(GPIBCommand::DCL);
+    }
+
+    pub fn format_disk(&mut self) {
+        self.low_level_format();
+        println!("Low level format complete!");
+
+        for i in 0..720 {
+            let sector = self.read_sector(i as u32);
+            assert_eq!(&sector[..8], &[0xff; 8], "bad sector after format");
+            assert_eq!(&sector[8..], &[0xe5; 504], "bad sector after format");
+            println!("Sector {}/720 verified", i+1);
+        }
+    }
+
+    fn low_level_format(&mut self) {
+        self.send_format_cmd();
+
+        self.serial_poll_handshake();
+
+        self.response_handshake();
+        assert_eq!(self.buffer[0], 0x00, "format failed");
+    }
+
+    pub fn read_disk_to_image(&mut self) {
+        panic!("TODO");
+    }
+
+    pub fn write_image_to_disk(&mut self, image: &[u8]) -> io::Result<()> {
+        if image.len() % SECTOR_SIZE != 0 {
+            return Err(io::ErrorKind::InvalidInput.into());
+        }
+
+        let total_sectors = image.len() / SECTOR_SIZE;
+        for i in 0..total_sectors {
+            let offset = i * SECTOR_SIZE;
+            let data = &image[offset..offset + SECTOR_SIZE];
+
+            self.write_sector(i as u32, data);
+
+            let sector = self.read_sector(i as u32);
+            assert_eq!(sector, data, "read write data mismatch");
+        }
+
+        Ok(())
+    }
+
+    fn write_sector(&mut self, sector: u32, data: &[u8]) {
+        self.send_write_cmd(sector, data);
+
+        self.serial_poll_handshake();
+
+        self.response_handshake();
+        assert!(self.buffer[0] == 0x00, "write failed {:#04x}", self.buffer[0]);
+    }
+
+    fn read_sector<'a>(&'a mut self, sector: u32) -> &'a [u8] {
+        self.send_read_cmd(sector);
+
+        self.serial_poll_handshake();
+
+        self.response_handshake();
+
+        if self.buffer.len() == 7 {
+            assert!(self.buffer[0] == 0x00, "read failed with status {:#04x}", self.buffer[0]);
+        }
+
+        assert_eq!(self.buffer.len(), SECTOR_SIZE, "weird `read` response length from device");
+        return &self.buffer;
+    }
+
+    fn send_format_cmd(&self) {
         let mut talker = Talker::new();
 
-        talker.send_command(GPIBCommand::DCL);
+        self.send_bytes_with_handshake(
+            &mut talker,
+            &disk_request::Request {
+                code: disk_request::RequestCode::Format,
+                connection: 0,
+                sector: 0,
+                data_size: 1,
+                mode: 0,
+            }
+            .into_bytes(),
+        );
+    }
+
+    fn send_write_cmd(&self, sector: u32, data: &[u8]) {
+        assert!(data.len() == SECTOR_SIZE);
+
+        let mut talker = Talker::new();
 
         self.send_bytes_with_handshake(
             &mut talker,
             &disk_request::Request {
                 code: disk_request::RequestCode::Write,
                 connection: 0,
-                sector: sector_number,
-                data_size: 512,
+                sector,
+                data_size: SECTOR_SIZE as _,
                 mode: 0,
             }
             .into_bytes(),
         );
 
-        self.send_bytes_with_handshake(&mut talker, sector_data);
+        self.send_bytes_with_handshake(&mut talker, data);
+    }
 
-        // println!("Wait srq...");
-        talker.wait_srq();
-        // println!("Got it!");
-
-        talker.send_command(GPIBCommand::SPE);
-
-        talker.send_command(GPIBCommand::MTA(self.address));
-        drop(talker);
-
-        self.read_serial_poll();
-
-        talker = Talker::new();
-
-        talker.send_command(GPIBCommand::SPD);
-        talker.send_command(GPIBCommand::UNT);
-
-        talker.send_command(GPIBCommand::MTA(self.address));
-        drop(talker);
-
-        self.read_and_println(buffer);
-        assert_eq!(buffer.len(), 7);
-        assert_eq!(buffer[0], 0x00);
-
-        // println!("Read after write...");
-
+    fn send_read_cmd(&self, sector: u32) {
         let mut talker = Talker::new();
-
-        talker.send_command(GPIBCommand::DCL);
 
         self.send_bytes_with_handshake(
             &mut talker,
             &disk_request::Request {
                 code: disk_request::RequestCode::Read,
                 connection: 0,
-                sector: sector_number,
-                data_size: 512,
+                sector,
+                data_size: SECTOR_SIZE as _,
                 mode: 0,
             }
             .into_bytes(),
         );
+    }
 
-        // println!("Wait srq...");
+    fn serial_poll_handshake(&self) {
+        let mut talker = Talker::new();
+
         talker.wait_srq();
-        // println!("Got it!");
 
         talker.send_command(GPIBCommand::SPE);
 
         talker.send_command(GPIBCommand::MTA(self.address));
         drop(talker);
 
-        self.read_serial_poll();
+        let mut listener = Listener::new(false);
+        assert_eq!(listener.handshake_byte().value, 0x4F);
+        drop(listener);
 
         talker = Talker::new();
 
         talker.send_command(GPIBCommand::SPD);
         talker.send_command(GPIBCommand::UNT);
+    }
 
+    fn response_handshake(&mut self) {
+        let mut talker = Talker::new();
         talker.send_command(GPIBCommand::MTA(self.address));
         drop(talker);
 
-        self.read_and_println(buffer);
-        if buffer.len() != 512 {
-            println!("Bad response: {:02x?} ({} bytes)", buffer, buffer.len());
+        self.buffer.clear();
+
+        let mut listener = Listener::new(false);
+        loop {
+            let byte = listener.handshake_byte();
+            self.buffer.push(byte.value);
+
+            if byte.eoi {
+                break;
+            }
         }
-        else if &buffer[0..512] != sector_data {
-            println!("Data mismatch");
-        }
-        buffer.clear();
+
+        drop(listener);
+
+        let mut talker = Talker::new();
+        talker.send_command(GPIBCommand::UNT);
+        drop(talker);        
     }
 
     #[inline(always)]
@@ -117,29 +193,5 @@ impl DeviceController {
         talker.send_command(GPIBCommand::MLA(self.address));
         talker.send_bytes(bytes, true);
         talker.send_command(GPIBCommand::UNL);
-    }
-
-    #[inline(always)]
-    fn read_serial_poll(&self) {
-        let mut listener = Listener::new(false);
-        listener.handshake_byte();
-        // println!("Serial poll byte {:02x}", byte.value);
-    }
-
-    #[inline(always)]
-    fn read_and_println(&self, buffer: &mut Vec<u8>) {
-        buffer.clear();
-
-        let mut listener = Listener::new(false);
-        loop {
-            let byte = listener.handshake_byte();
-            buffer.push(byte.value);
-
-            if byte.eoi {
-                break;
-            }
-        }
-
-        // println!("Buffer: {:02x?} ({} bytes)", buffer, buffer.len());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,31 @@
 mod args;
 mod controller;
 mod emulator;
-mod gpib;
-mod gpio;
+mod gpib_command;
+mod gpib_gpio;
+mod gpib_pinout;
+mod listener;
 mod logger;
 mod sniffer;
-mod system;
-mod time_utils;
+mod talker;
+mod utils;
 
-use std::{fs, io, path::Path, process::ExitCode};
+use std::{fs, io, path::Path};
 
 use crate::{
     args::{Args, ControllerArgs, EmulatorArgs, SnifferArgs},
     controller::DeviceController,
     emulator::DeviceEmulator,
-    gpio::Gpio,
     logger::LogLevel,
     sniffer::BusSniffer,
-    system::{DeviceInfo, GpioInterface},
+    utils::configure_scheduler,
 };
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-fn main() -> ExitCode {
+fn main() {
     let args = Args::parse();
 
-    setup_logger(&args);
-
-    match blackgpib(args) {
-        Ok(()) => {
-            return ExitCode::SUCCESS;
-        }
-        Err(err) => {
-            error!("BlackGPiB fatal error: {err}");
-            return ExitCode::FAILURE;
-        }
-    }
-}
-
-fn setup_logger(args: &Args) {
     logger::setup(if args.trace {
         LogLevel::Trace
     } else if args.verbose {
@@ -48,187 +35,106 @@ fn setup_logger(args: &Args) {
     } else {
         LogLevel::Info
     });
-}
 
-fn blackgpib(args: Args) -> io::Result<()> {
-    check_device_compatibility()?;
-    let gpio = open_gpio()?;
+    info!("BlackGPiB v{VERSION} started");
 
-    configure_scheduler()?;
+    debug!("Reset all pins to Z-State...");
+    gpib_gpio::reset_all();
 
     match args.command {
-        args::Command::Emulator(args) => run_emulator(args, gpio),
-        args::Command::Sniffer(args) => run_sniffer(args, gpio),
+        args::Command::Emulator(args) => run_emulator(args),
+        args::Command::Sniffer(args) => run_sniffer(args),
         args::Command::Controller(args) => run_controller(args),
     }
 }
 
-fn check_device_compatibility() -> io::Result<()> {
-    debug!("Check device compatibility");
-
-    match DeviceInfo::new() {
-        Ok(info) => match info.gpio_interface() {
-            GpioInterface::Bcm => {
-                info!("Detected supported {} ({})", info.model(), info.soc());
-                return Ok(());
-            }
-            GpioInterface::Rp1 => {
-                info!("Sorry, your {} ({}) does not supported :(", info.model(), info.soc());
-                return Err(io::Error::new(io::ErrorKind::Unsupported, "RP1 gpio does not supported"));
-            }
-        },
-        Err(err) if err.kind() == io::ErrorKind::Unsupported => {
-            return Err(io::Error::new(io::ErrorKind::Unsupported, "Unknown or unsupported Raspberry Pi model"));
-        }
-        Err(err) => {
-            return Err(err);
-        }
-    }
-}
-
-fn open_gpio() -> io::Result<Gpio> {
-    debug!("Open GPIO");
-
-    // SAFETY: Initialized only once; no other GPIO exist.
-    unsafe { Gpio::new() }
-}
-
-fn configure_scheduler() -> io::Result<()> {
-    trace!("Pin blackgpib to core 3 and set priority");
-
-    // SAFETY: CPU affinity mask is configured per the documentation.
-    let cpu_mask = unsafe {
-        let mut set = std::mem::zeroed();
-        libc::CPU_ZERO(&mut set);
-        libc::CPU_SET(3, &mut set);
-        set
-    };
-
-    // SAFETY: This call does not cause UB; on error it returns -1.
-    let result = unsafe { libc::sched_setaffinity(0, size_of_val(&cpu_mask), &cpu_mask) };
-    if result == -1 {
-        return Err(io::Error::last_os_error());
-    }
-
-    // SAFETY: This call does not cause UB; on error it returns -1.
-    // let result = unsafe { libc::setpriority(libc::PRIO_PROCESS, 0, -19) };
-    // if result == -1 {
-    //     return Err(io::Error::last_os_error());
-    // }
-
-    Ok(())
-}
-
-fn run_emulator(args: EmulatorArgs, gpio: Gpio) -> io::Result<()> {
+fn run_emulator(args: EmulatorArgs) {
     let mut emulator = DeviceEmulator::new();
+    configure_emulator(args, &mut emulator);
 
-    debug!("Configure emulator before start");
-    configure_emulator(args, &mut emulator)?;
+    configure_scheduler();
 
-    info!("Start BlackGPiB v{VERSION} emulator");
-    emulator.start(gpio);
-
-    Ok(())
+    debug!("Configuration complete, start device emulator");
+    emulator.start();
 }
 
-fn configure_emulator(args: EmulatorArgs, emulator: &mut DeviceEmulator) -> io::Result<()> {
+fn configure_emulator(args: EmulatorArgs, emulator: &mut DeviceEmulator) {
     emulator.create_proxy(21, 49274); // default printer
     emulator.create_proxy(25, 49275); // printer hp
     emulator.create_proxy(20, 49276); // plotter
 
     if let Some(ref path) = args.hdd_1_image {
-        emulator.create_disk(04, mmap_disk_image(path)?);
+        emulator.create_disk(04, mmap_disk_image(path));
     }
     if let Some(ref path) = args.floppy_drive_1_image {
-        emulator.create_disk(05, mmap_disk_image(path)?);
+        emulator.create_disk(05, mmap_disk_image(path));
     }
     if let Some(ref path) = args.portable_floppy_image {
-        emulator.create_disk(06, mmap_disk_image(path)?);
+        emulator.create_disk(06, mmap_disk_image(path));
     }
     if let Some(ref path) = args.hdd_2_image {
-        emulator.create_disk(12, mmap_disk_image(path)?);
+        emulator.create_disk(12, mmap_disk_image(path));
     }
     if let Some(ref path) = args.floppy_drive_2_image {
-        emulator.create_disk(13, mmap_disk_image(path)?);
+        emulator.create_disk(13, mmap_disk_image(path));
     }
-
-    Ok(())
 }
 
-fn mmap_disk_image(path: &Path) -> io::Result<memmap2::MmapMut> {
-    let map_err = |err: io::Error, action: &str| {
-        io::Error::new(err.kind(), format!("failed to {action} file {}: {}", path.display(), err))
-    };
-
-    debug!("Open disk image {}", path.display());
-
+fn mmap_disk_image(path: &Path) -> memmap2::MmapMut {
     let file = fs::OpenOptions::new()
         .read(true)
         .write(true)
         .create(false)
         .truncate(false)
         .open(path)
-        .map_err(|err| map_err(err, "open"))?;
+        .expect(&format!("failed to open image {}", path.display()));
 
-    file.lock().map_err(|err| map_err(err, "lock"))?;
+    // SAFETY: Maybe safe, I don't know.
+    let mmap = unsafe { memmap2::MmapMut::map_mut(&file) };
 
-    // SAFETY: The file is opened and locked; no other process can access it.
-    let mmap = unsafe { memmap2::MmapMut::map_mut(&file) }.map_err(|err| map_err(err, "mmap"))?;
-
-    // The file is needed for the entire emulator lifetime; it must not be dropped.
-    std::mem::forget(file);
-
-    return Ok(mmap);
+    return mmap.expect(&format!("failed to mmap image {}", path.display()));
 }
 
-fn run_sniffer(args: SnifferArgs, gpio: Gpio) -> io::Result<()> {
-    let file = create_dump_file(&args.output_path, args.size)?;
+fn run_sniffer(args: SnifferArgs) {
+    let file = create_dump_file(&args.output_path, args.size).expect("failed to create dump file");
     let sniffer = BusSniffer::new(file);
 
-    info!("Start BlackGPiB v{VERSION} sniffer");
+    configure_scheduler();
 
-    sniffer.start(gpio);
+    debug!("Configuration complete, start bus sniffer");
+    sniffer.start();
 
     info!("Bus sniffer finished, dump saved to {}", args.output_path.display());
-
-    Ok(())
 }
 
 fn create_dump_file(path: &Path, size: usize) -> io::Result<memmap2::MmapMut> {
-    let map_err = |err: io::Error, action: &str| {
-        io::Error::new(err.kind(), format!("failed to {action} file {}: {}", path.display(), err))
-    };
-
-    let file_exists = fs::exists(path).map_err(|err| map_err(err, "check"))?;
-    if file_exists {
-        return Err(io::Error::new(io::ErrorKind::AlreadyExists, format!("file {} already exists", path.display())));
+    if fs::exists(path)? {
+        return Err(io::ErrorKind::AlreadyExists.into());
     }
 
     let file = fs::OpenOptions::new()
         .read(true)
         .write(true)
         .create_new(true)
-        .open(path)
-        .map_err(|err| map_err(err, "open"))?;
+        .open(path)?;
 
-    file.set_len(size as u64).map_err(|err| map_err(err, "set length of"))?;
+    file.set_len(size as u64)?;
 
-    file.lock().map_err(|err| map_err(err, "lock"))?;
-
-    // SAFETY: The file is opened and locked; no other process can access it.
-    let mmap = unsafe { memmap2::MmapMut::map_mut(&file) }.map_err(|err| map_err(err, "mmap"))?;
-
-    // The file is needed for the entire sniffer lifetime; it must not be dropped.
-    std::mem::forget(file);
-
-    return Ok(mmap);
+    // SAFETY: Maybe safe, I don't know.
+    unsafe { memmap2::MmapMut::map_mut(&file) }
 }
 
-fn run_controller(args: ControllerArgs) -> io::Result<()> {
-    let controller = DeviceController::new(args.address);
+fn run_controller(args: ControllerArgs) {
+    let mut controller = DeviceController::new(args.address);
 
-    info!("Start BlackGPiB v{VERSION} controller");
-    controller.start();
+    configure_scheduler();
 
-    Ok(())
+    debug!("Configuration complete");
+
+    // let image = fs::read("MS-DOS 2.00A.img").unwrap();
+
+    // controller.reset_device();
+    // println!("Reset complete!");
+    // controller.format_disk();
+    // controller.write_image_to_disk(&image).unwrap();
 }
